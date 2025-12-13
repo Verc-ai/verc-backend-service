325a8dd19198ac585ba026b601fc612b5f34e05e
hakangunturkun <gunturkunhakan@gmail.com>
2025-12-12 22:27:24 -0800

    staging test

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -51,7 +51,7 @@ class LoginView(APIView):
             try:
                 # Authenticate with Supabase
                 auth_response = supabase_auth.auth.sign_in_with_password({
-                    'email': email,
+                    'email_133': email,
                     'password': password
                 })
                 
